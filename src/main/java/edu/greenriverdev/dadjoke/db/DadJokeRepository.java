1d98574f5e178942151f1a77df78a6884c1e87cc
Nathan <nathanwaters94@gmail.com>
2024-01-18 14:26:10 -0800

    step 4 complete: setting up repository and connecting to temp database

---
+++ src/main/java/edu/greenriverdev/dadjoke/db/DadJokeRepository.java
@@ -0,0 +1,8 @@
+package edu.greenriverdev.dadjoke.db;
+
+import edu.greenriverdev.dadjoke.domain.DadJokes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DadJokeRepository extends JpaRepository<DadJokes, Integer> {
+
+}
